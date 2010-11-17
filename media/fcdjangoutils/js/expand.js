f5af9ba607229d326fdf63e4b55bbdb9a10cce1e
Egil Moeller <egil.moller@freecode.no>
2010-11-17 11:09:47 +0100

    Added a toggle-button class toggle

--- media/fcdjangoutils/js/expand.js
+++ media/fcdjangoutils/js/expand.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $('.autohide-body').hide();
   $('.autohide-button').click(function () {
     var id = this.id.replace(new RegExp("-button$"), "");
+    $(this).toggleClass('autohide-expanded');
     $('#' + id + '-body').toggle(500);
   });
 });
