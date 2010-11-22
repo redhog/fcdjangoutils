edb0f247764560efeb574d63d1973f54155ae984
Axel Liljencrantz <axel.liljencrantz@freecode.no>
2010-11-22 10:47:29 +0100

    Add transaction with-statement helper thingie

---
+++ db.py
@@ -0,0 +1,21 @@
+
+from django.db import transaction
+
+class Transaction(object):
+    def __init__(self, db=None):
+        self.db = db
+
+    def __enter__(self):
+#        print "Entering a transaction"
+        transaction.enter_transaction_management(using=self.db)
+
+    def __exit__(self, a,b,c):
+        if a is None:
+#            print "Commiting transaction"
+            transaction.commit(using=self.db)
+        else:
+            # TODO: Might be nice to log all DB rollbacks...
+
+#            print "Rolling back transaction"
+            transaction.rollback(using=self.db)
+
