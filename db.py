38fe78ad46d720ba3dc2d4a44e5a7d66d34d9459
Axel Liljencrantz <axel.liljencrantz@freecode.no>
2010-11-22 15:07:43 +0100

    Transaction wrapper was broken. Actually works now.

--- db.py
+++ db.py
@@ -6,16 +6,17 @@ class Transaction(object):
         self.db = db
 
     def __enter__(self):
-#        print "Entering a transaction"
+        print "Entering a transaction"
         transaction.enter_transaction_management(using=self.db)
+        transaction.managed(True, using=self.db)
 
     def __exit__(self, a,b,c):
         if a is None:
-#            print "Commiting transaction"
+            print "Commiting transaction"
             transaction.commit(using=self.db)
         else:
             # TODO: Might be nice to log all DB rollbacks...
 
-#            print "Rolling back transaction"
+            print "Rolling back transaction"
             transaction.rollback(using=self.db)
 
