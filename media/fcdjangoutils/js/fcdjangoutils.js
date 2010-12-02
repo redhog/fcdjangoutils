09d18a414522f6ef32901e0464f134150d6b48bf
Reidar Øksnevad <reidar.oksnevad@freecode.no>
2010-12-02 15:09:44 +0100

    Pad iso dates.

--- media/fcdjangoutils/js/fcdjangoutils.js
+++ media/fcdjangoutils/js/fcdjangoutils.js
@@ -85,7 +85,9 @@ fcdjangoutils.fromIsoDate = function(txt){
 };
 
 fcdjangoutils.toIsoDate = function (date){
-    return "" + date.getFullYear() + "-" + (date.getMonth()+1)+"-" + date.getDate();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    return "" + date.getFullYear() + "-" + ((month < 10 ? '0' : '') + month) + "-" + ((day < 10 ? '0' : '') + day);
 }
 
 /**
