2ad42270e2bff8323296f6e66654398bc1dc7fa2
Reidar Øksnevad <reidar.oksnevad@freecode.no>
2011-01-12 10:13:15 +0100

    Comments functionality.

--- media/fcdjangoutils/js/expand.js
+++ media/fcdjangoutils/js/expand.js
@@ -1,8 +1,18 @@
-$(document).ready(function () {
-  $('.autohide-body').hide();
+expand = {};
+
+expand.prepare = function () {
+  //$('.autohide-body').hide();
   $('.autohide-button').click(function () {
     var id = this.id.replace(new RegExp("-button$"), "");
     $(this).toggleClass('autohide-expanded');
     $('#' + id + '-body').toggle(500);
   });
-});
+
+  $('.new_comment').focus(function() {
+      var comment = $(this);
+      comment.addClass('expanded', 200);
+      comment.parent().next('button.submit_comment').show();
+    });
+}
+
+$(document).ready(expand.prepare);
