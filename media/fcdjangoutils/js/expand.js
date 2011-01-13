2c75c1472d0917a1ce94c14733d8dc05958e01e0
Axel Liljencrantz <axel.liljencrantz@freecode.no>
2011-01-13 12:44:19 +0100

    Fix autoexpand bug
    refs #3487

--- media/fcdjangoutils/js/expand.js
+++ media/fcdjangoutils/js/expand.js
@@ -2,17 +2,17 @@ expand = {};
 
 expand.prepare = function () {
   //$('.autohide-body').hide();
-  $('.autohide-button').click(function () {
+  $('.autohide-button:not(.autohide-init)').addClass('autohide-init').click(function () {
     var id = this.id.replace(new RegExp("-button$"), "");
     $(this).toggleClass('autohide-expanded');
     $('#' + id + '-body').toggle(500);
   });
 
-  $('.new_comment').focus(function() {
+  $('.new_comment:not(.new-comment-init)').addClass('new-comment-init').focus(function() {
       var comment = $(this);
       comment.addClass('expanded', 200);
       comment.parent().next('button.submit_comment').show();
     });
-}
+};
 
 $(document).ready(expand.prepare);
