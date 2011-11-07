3d27af34cc14c5b22db52e8520d927772afa157d
Kim Tore Jensen <kim.tore@freecode.no>
2011-11-07 13:23:08 +0100

    Add 'active' template tag

---
+++ templatetags/active.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Note, this library is free software
+# Copyright 2010, 2011 FreeCode AS
+# Licensed under the MIT license
+
+from django import template
+from django.core.urlresolvers import reverse
+import re
+
+register = template.Library()
+
+@register.simple_tag
+def active(request, pattern):
+    if re.match(pattern, request.path):
+        return "active"
+    return ""
