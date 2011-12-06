1f6e2ba468f18049d044b489762e979f4f77dbdd
Eivind Øksnevad <eivind.oksnevad@freecode.no>
2011-12-06 12:32:12 +0100

    Added tag to get the currently active app

--- templatetags/active.py
+++ templatetags/active.py
@@ -15,3 +15,11 @@ def active(request, pattern):
     if re.match(pattern, request.path):
         return "active"
     return ""
+
+@register.simple_tag
+def current_app(request):
+    match = re.findall('\/([^\/]+)\/', request.path)
+    if match:
+        return match[0]
+    return ''
+
